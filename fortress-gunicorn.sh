a906339621c467d1d73cf7a4ae333b1d11848f4e
yudanta <me@yudanta.web.id>
2020-12-09 10:47:13 +0700

    change for heroku deploy

--- fortress-gunicorn.sh
+++ fortress-gunicorn.sh
@@ -1,2 +1,2 @@
-# gunicorn --name 'DB CMS WEB' --chdir /home/fortress/src --timeout 300 -b 0.0.0.0:$PORT app:app -k gevent --worker-connections 5 --workers 1 --reload
-gunicorn --name 'DB CMS WEB' --chdir /home/fortress/src --timeout 300 -b 0.0.0.0:8000 app:app -k gevent --worker-connections 5 --workers 1 --reload
+gunicorn --name 'DB CMS WEB' --chdir /home/fortress/src --timeout 300 -b 0.0.0.0:$PORT app:app -k gevent --worker-connections 5 --workers 1 --reload
+# gunicorn --name 'DB CMS WEB' --chdir /home/fortress/src --timeout 300 -b 0.0.0.0:8000 app:app -k gevent --worker-connections 5 --workers 1 --reload
